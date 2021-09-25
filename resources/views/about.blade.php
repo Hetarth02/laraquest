9c41481c5567d50fb09b4366af9455acd7d4d308
Hetarth02 <hetarth02@gmail.com>
2021-09-26 02:16:03 +0530

    updated background color

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -28,6 +28,23 @@
         </div>
     </nav>
 
+    <div class="flexbox-container">
+        <div>
+            <h3>What is Laraquest?</h3>
+            <img class="logo" src="{{ URL::asset('images/blue_logo.png') }}">
+            <p>Laraquest is a social support forum platform where users can create forums, threads and reply to different threads. And if they want they could also subscribe to any specific thread to receive daily updates about the thread. It is an Open-Source Project made by Hetarth Shah.</p>
+            <h4>Tech Stack:</h4>
+            <p>
+                Framworks: Laravel
+                Languages: HTML, CSS, JS, PHP
+                External Resources: Bootstrap 4/5, Google Fonts and Icons
+                Database: MySQL
+                Version Control: Git/Github
+            </p>
+        </div>
+        <div></div>
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
